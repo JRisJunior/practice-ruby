7d4f37aefdd074137e28686628515829d2b537a9
JRisJunior <jeffmrubin@gmail.com>
2022-11-29 14:23:30 -0600

    remove old defs and fix prints

--- store_item.rb
+++ store_item.rb
@@ -21,32 +21,12 @@ class StoreItem
     @price = input_tool[:price]
   end
 
-  def name
-    @name
-  end
-
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
-  def change_color=(new_color)
-    @color = new_color
-  end
-
-  def change_price=(new_price)
-    @price = new_price
-  end
-
 end
 
 p item1 = StoreItem.new(name: "wrench", color: "silver", price: 7.00)
 
-p item1.change_color=("brown")
-p item1.change_price=(10.0)
+p item1.color = "brown"
+p item1.price = 10.0
 p item1.name
 p item1.color
 p item1.price
