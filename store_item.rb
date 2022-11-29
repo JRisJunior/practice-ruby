f8668cfe81bced1f4e25c31a37afbba909b17149
JRisJunior <jeffmrubin@gmail.com>
2022-11-29 13:57:58 -0600

    add reader and writer for color and change

--- store_item.rb
+++ store_item.rb
@@ -10,13 +10,35 @@
 
 # p item3
 
-class Item
-  def initialize(add_name, add_color, add_cost)
+class StoreItem
+  def initialize(add_name, add_color, add_price)
     @name = add_name
     @color = add_color
-    @cost = add_cost
+    @price = add_price
   end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def change_color=(new_color)
+    @color = new_color
+  end
+
+  def change_price=(new_price)
+    @price = new_price
+  end
+
 end
 
-p Item.new("wrench", "silver", 7.00)
+p item1 = StoreItem.new("wrench", "silver", 7.00)
 
+p item1.change_color=("brown")
