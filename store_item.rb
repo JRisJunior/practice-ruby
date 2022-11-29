847f9e64a7a2d8b64c1c85abc51d2d7dfd5e82b4
JRisJunior <jeffmrubin@gmail.com>
2022-11-29 13:51:37 -0600

    change hash to class

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,22 @@
-item1 = {name: "wrench", color: "silver", price: 7.00}
+# item1 = {name: "wrench", color: "silver", price: 7.00}
 
-p item1
+# p item1
 
-item2 = {:name => "hammer", :color => "black", :price => 5.00}
+# item2 = {:name => "hammer", :color => "black", :price => 5.00}
 
-p item2
+# p item2
 
-item3 = {'name': "screwdriver", 'color': 'red', 'price': 3.00}
+# item3 = {'name': "screwdriver", 'color': 'red', 'price': 3.00}
 
-p item3
+# p item3
+
+class Item
+  def initialize(add_name, add_color, add_cost)
+    @name = add_name
+    @color = add_color
+    @cost = add_cost
+  end
+end
+
+p Item.new("wrench", "silver", 7.00)
+
